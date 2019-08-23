715f557a312c4dc5771c28680a0cde05f96e4628
彭祥智 <326104789@qq.com>
2019-08-23 19:45:23 +0800

    Update pxz.js

--- pxz.js
+++ pxz.js
@@ -53,3 +53,35 @@ function tap(element,fn){
     fn && fn();
   })
 }
+// 获取一个随机的rgb格式的颜色
+function randomColor(){
+  var r = Math.floor(Math.random()*256);
+  var g = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
+  return "rgb("+r+","+g+","+b+")";
+}
+
+//获取十六进制颜色
+function randomColor1(){
+  var r = Math.floor(Math.random()*256);
+  var g = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
+   
+  if(r < 16){
+      r = "0"+r.toString(16);
+  }else{
+      r = r.toString(16);
+  }
+  if(g < 16){
+      g = "0"+g.toString(16);
+  }else{
+      g = g.toString(16);
+  }
+  if(b < 16){
+      b = "0"+b.toString(16);
+  }else{
+      b = b.toString(16);
+  }
+   
+  return "#"+r+g+b;
+}
